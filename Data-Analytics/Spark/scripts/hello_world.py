5404e03b279f9ce4f83fe01c961a800e04b343a4
Andrew Mendez <interactivetech@users.noreply.github.com>
2025-04-02 17:08:45 -0400

    test non spark job

--- Data-Analytics/Spark/scripts/hello_world.py
+++ Data-Analytics/Spark/scripts/hello_world.py
@@ -1,23 +1,23 @@
 # Data-Engineering/Airflow/scripts/hello_world.py
-# import time
+import time
 
-# print("Hello World from the Airflow DAG!")
-# print("Sleeping for 10 seconds...")
-# time.sleep(10)
-# print("Done.")
-from pyspark.sql import SparkSession
+print("Hello World from the Airflow DAG!")
+print("Sleeping for 10 seconds...")
+time.sleep(10)
+print("Done.")
+# from pyspark.sql import SparkSession
 
-if __name__ == "__main__":
-    spark = SparkSession \
-        .builder \
-        .appName("HelloWorldSpark") \
-        .getOrCreate()
+# if __name__ == "__main__":
+#     spark = SparkSession \
+#         .builder \
+#         .appName("HelloWorldSpark") \
+#         .getOrCreate()
 
-    # Create a simple RDD and collect it
-    rdd = spark.sparkContext.parallelize(["Hello, world"])
-    result = rdd.collect()
+#     # Create a simple RDD and collect it
+#     rdd = spark.sparkContext.parallelize(["Hello, world"])
+#     result = rdd.collect()
 
-    for line in result:
-        print(line)
+#     for line in result:
+#         print(line)
 
-    spark.stop()
+#     spark.stop()
