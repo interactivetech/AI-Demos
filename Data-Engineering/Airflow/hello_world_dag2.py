2832297a6cf1be65a1ddfe886ec28963854699f1
Andrew Mendez <interactivetech@users.noreply.github.com>
2025-04-03 13:55:54 -0400

    update

--- Data-Engineering/Airflow/hello_world_dag2.py
+++ Data-Engineering/Airflow/hello_world_dag2.py
@@ -1,9 +1,10 @@
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from airflow import DAG
 from airflow.utils.dates import days_ago
+from kubernetes.client import models as k8s
 
 from airflow.models.param import Param
-#v0.0.6
+#v0.0.7
 
 default_args = {
     "owner": "airflow",
