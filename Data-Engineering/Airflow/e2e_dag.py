08ca1f771b7cd432eec3d99fb97600892bf99e42
Andrew Mendez <interactivetech@users.noreply.github.com>
2025-05-09 17:14:26 -0400

    050925_010_download_logs

--- Data-Engineering/Airflow/e2e_dag.py
+++ Data-Engineering/Airflow/e2e_dag.py
@@ -64,7 +64,6 @@ download_log = KubernetesPodOperator(
     labels={"app": "minio-downloader"},
     do_xcom_push=False,
     volumes=[volume],
-    enable_impersonation_from_ldap_user=True,
     volume_mounts=[volume_mount],
     container_resources={
         "requests": {"memory": "128Mi", "cpu": "100m"},
