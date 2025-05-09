a4abe7f53c23b0326182a848ba7184f04b07f4aa
Andrew Mendez <interactivetech@users.noreply.github.com>
2025-05-09 17:07:32 -0400

    050925_007_download_logs

--- Data-Engineering/Airflow/e2e_dag.py
+++ Data-Engineering/Airflow/e2e_dag.py
@@ -3,6 +3,8 @@ from airflow.utils.dates import days_ago
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 from kubernetes.client import models as k8s
 
+from airflow.models.param import Param
+
 # DAG arguments
 default_args = {
     "owner": "airflow",
