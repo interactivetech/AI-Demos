eba81f91b073db7ffe696d6b7fb6b2b94e2b3501
Andrew Mendez <interactivetech@users.noreply.github.com>
2025-05-13 06:45:53 -0400

    051225_047_download_logs

--- Data-Analytics/Spark/scripts/train_and_export_model.py
+++ Data-Analytics/Spark/scripts/train_and_export_model.py
@@ -29,7 +29,9 @@ try:
         print("✅ MLflow token injected into environment")
 except Exception as e:
     raise RuntimeError(f"❌ Failed to read MLflow token: {e}")
-
+os.environ['MLFLOW_TRACKING_INSECURE_TLS']=True
+os.environ['MLFLOW_S3_IGNORE_TLS']=True
+os.environ['MLFLOW_S3_ENDPOINT_URL']='http://local-s3-service.ezdata-system.svc.cluster.local:30000'
 # --- Helper: Upload a folder to MinIO ---
 def upload_folder(client, bucket, local_folder, prefix):
     for root, dirs, files in os.walk(local_folder):
