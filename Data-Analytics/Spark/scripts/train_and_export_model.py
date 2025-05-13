45b1a2125881d98de7aa3884ff843b123280e800
Andrew Mendez <interactivetech@users.noreply.github.com>
2025-05-13 08:04:06 -0400

    051325_056_download_logs

--- Data-Analytics/Spark/scripts/train_and_export_model.py
+++ Data-Analytics/Spark/scripts/train_and_export_model.py
@@ -37,6 +37,7 @@ os.environ['MLFLOW_TRACKING_INSECURE_TLS']='true'
 os.environ['MLFLOW_S3_IGNORE_TLS']='true'
 os.environ['MLFLOW_S3_ENDPOINT_URL']='http://local-s3-service.ezdata-system.svc.cluster.local:30000'
 os.environ['AWS_ENDPOINT_URL']='http://local-s3-service.ezdata-system.svc.cluster.local:30000'
+os.environ['AUTH_TOKEN'] = os.environ['MLFLOW_TRACKING_TOKEN']
 # --- Helper: Upload a folder to MinIO ---
 def upload_folder(client, bucket, local_folder, prefix):
     for root, dirs, files in os.walk(local_folder):
